8106428786ce084ae7b4cca3f8963843d3f013b8
ErikRidhoFirm <erikridh1@gmail.com>
2023-11-10 09:16:21 +0700

    jobsheet semester

---
+++ Jobsheet-3/ContohVariabel09.java
@@ -0,0 +1,14 @@
+public class ContohVariabel09{
+    public static void main(String args[]) {
+        String salahSatuHobySayaAdalah = "Mendengarkan musik";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 20;
+        double $ipk = 3.88, tinggi = 1.90;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);   
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
---
+++ jobsheet-8 (UTS)/Pembayaran09.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Pembayaran09{
+    public static void main(String[] args){
+        Scanner input09 = new Scanner(System.in);
+        String jenisBarang = 0, nama = 0;
+        int harga;
+        System.out.print("Masukkan Jumlah Buah :");
+    }
+}
